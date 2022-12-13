c50e635fb6b24f19d73795d158088fec5db917f6
Moein Arabi <moein.mo81@gmail.com>
2022-12-13 15:20:58 +0330

    Refactor classes

---
+++ src/PrimeNumberAbstract.java
@@ -0,0 +1,56 @@
+public abstract class PrimeNumberAbstract implements PrimeNumber{
+    @Override
+    public boolean isPrimeNumber1(int n) {
+        if (n == 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    @Override
+    public boolean isPrimeNumber2(int n) {
+        if (n == 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    @Override
+    public int countPrimeNumbers4(int end) {
+        boolean[] notPrime = new boolean[end + 1];
+        notPrime[1] = true;
+        for (int i = 2; i <= end; i++) {
+            if (!notPrime[i]) {
+                for (int j = 2; i * j <= end; j++) {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+        
+        int result = 0;
+        for (int i = 1; i <= end; i++) {
+            if (!notPrime[i]) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
