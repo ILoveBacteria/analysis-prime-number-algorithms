8c94f9fbe5f5e9d4ade8846d48b5d72e044e162f
Moein Arabi <moein.mo81@gmail.com>
2022-12-13 17:36:29 +0330

    Update test cases

--- src/TestCaseGenerator.java
+++ src/TestCaseGenerator.java
@@ -6,10 +6,13 @@ public class TestCaseGenerator {
     private static final int MAX_INPUT = 1_000_000;
     public static void generate() throws IOException {
         try (FileWriter writer = new FileWriter(FILE_PATH)) {
-            int i = 1, j = 10;
+            for (int i = 1; i < 1000; i++) {
+                writer.write(String.format("%d\n", i));
+            }
+            int i = 1000, j = 10_000;
             while (j <= MAX_INPUT) {
-                for (int k = i; k < j; k += i) {
-                    int n = ((int) (Math.random() * j)) % i + k;
+                for (int k = i; k < j; k += i / 2) {
+                    int n = ((int) (Math.random() * i / 2)) + k;
                     writer.write(String.format("%d\n", n));
                 }
                 i *= 10;
